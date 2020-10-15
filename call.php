e2ef67d6ab9757213eb1debc0b0eff0c8c8b102b
Zhassulan96 <zhassulan96@mail.ru>
2020-10-15 23:36:39 +0500

    v6

--- call.php
+++ call.php
@@ -6,4 +6,5 @@ $msg = $_REQUEST['phone'];
 ?>
 Спасибо за заявку. Мы свяжемся по номеру: <?php
 echo $msg;
+mail('mahanbetov777@gmail.com',"zakaz balkon","tel:".$msg);
 mail('zhassulan_96@mail.ru',"zakaz balkon","tel:".$msg);
